7e0974c16469630021c17d745107c6aadf2d50fb
Haykes <haykes.pro@gmail.com>
2023-04-06 09:39:54 +0200

    Ajout avant le pull

--- app/app.js
+++ app/app.js
@@ -13,10 +13,10 @@ app.use(express.urlencoded({ extended: true }));
 // app.use("/clients", clientsRouter);
 
 app.use((req, res, next) => {
-	res.status(404).send({ error: req.originalUrl + " not found" });
+  res.status(404).send({ error: req.originalUrl + " not found" });
 });
 
 // NOTE - Start server
 app.listen(3000, "0.0.0.0", () => {
-	console.log("server started !");
+  console.log("server started !");
 });
